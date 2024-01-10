1bf7df05ab84ab03e3eaaf50b10bad19c62e8bb7
Aidan K <ak@umorpha.io>
2024-01-09 20:06:10 -0700

    created hw1 	modified:   Intermediate/Advanced Memory Management/deref_coercion/Cargo.toml 	new file:   Intermediate/Advanced Memory Management/deref_coercion/src/hw1.rs

---
+++ Intermediate/Advanced Memory Management/deref_coercion/src/hw1.rs
@@ -0,0 +1,29 @@
+// Complete the code by addressing the TODOs.
+
+use std::rc::Rc;
+
+struct Employee {
+    name: String,
+    id: u32,
+}
+
+impl Employee {
+    fn new(name: &str, id: u32) -> Self {
+        Employee {
+            name: name.to_string(),
+            id,
+        }
+    }
+    fn print_details(&self) {
+        println!("Name: {}, ID: {}", self.name, self.id);
+    }
+}
+
+fn main() {
+    let emp1 = Box::new(Employee::new("Alice", 1234));
+    // TODO: call print_details on emp1
+    let emp2 = Box::new(emp1);
+    // TODO: call print_details on emp2
+    let emp3 = Rc::new(emp2);
+    // TODO: call print_details on emp3
+}
