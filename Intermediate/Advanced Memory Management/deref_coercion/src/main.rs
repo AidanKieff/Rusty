bb9cd21032a681417fc0773780ca63e5ba4cec61
Aidan K <ak@umorpha.io>
2024-01-09 19:38:29 -0700

    finished deref_coercion modified:   Intermediate/Advanced Memory Management/deref_coercion/src/main.rs

--- Intermediate/Advanced Memory Management/deref_coercion/src/main.rs
+++ Intermediate/Advanced Memory Management/deref_coercion/src/main.rs
@@ -4,10 +4,27 @@ struct MysmartPointer<T> {
     value: T
 }
 
+impl<T> MysmartPointer<T> {
+    fn new(value: T) -> MysmartPointer<T> {
+        MysmartPointer { value }
+    }
+}
 
+impl<T> Deref for MysmartPointer<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.value
+    }
+}
+impl<T> DerefMut for MysmartPointer<T> {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.value
+    }
+}
 
 fn main() {
-    let s = Box::new("let's get it".to_owned());
+    let s = MysmartPointer::new(Box::new("let's get it".to_owned()));
 
     print(&s);
 
