0d24da3eb918fa59ea68f35594f4daa59ef2eecb
Aidan K <ak@umorpha.io>
2024-01-15 20:38:01 -0700

    finished error handling hw

--- Intermediate/understanding error handling in rust/recoverable_errors/src/lib.rs
+++ Intermediate/understanding error handling in rust/recoverable_errors/src/lib.rs
@@ -1,5 +1,7 @@
 //HW 1-3
 
+
+/*
 //------------------------start of hw1------------------------------
 // Rewrite the find_pos function.
 // The return type must be Result<usize, Error>.
@@ -83,3 +85,78 @@ mod tests {
         assert!(matches!(find_pos("I luv Rust", "uv"), Ok(3)));
     }
 }
+
+*/
+
+/*
+//---------------------------------start of hw2-------------------------
+
+
+pub fn generate_nametag_text(name: String) -> Result<String, String> {
+    if name.is_empty() {
+        Err("`name` was empty; it must be nonempty.".to_string())
+    } else {
+        Ok(format!("Hi! My name is {}", name))
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn generates_nametag_text_for_a_nonempty_name() {
+        assert_eq!(
+            generate_nametag_text("Beyoncé".into()),
+            Ok("Hi! My name is Beyoncé".into())
+        );
+    }
+
+    #[test]
+    fn explains_why_generating_nametag_text_fails() {
+        assert_eq!(
+            generate_nametag_text("".into()),
+            // Don't change this line
+            Err("`name` was empty; it must be nonempty.".into())
+        );
+    }
+}
+
+*/
+
+// ------------------------------start of hw 3 -----------------------------------
+/*
+// Complete the `new` associated function to check for invalid input.
+
+#[derive(PartialEq, Debug)]
+struct PositiveNonzeroInteger(u64);
+
+#[derive(PartialEq, Debug)]
+enum CreationError {
+    Negative,
+    Zero,
+}
+
+impl PositiveNonzeroInteger {
+    fn new(value: i64) -> Result<PositiveNonzeroInteger, CreationError> {
+        // Hmm...? Why is this only returning an Ok value?
+        match value{
+            i64::MIN..=-1 => Err(CreationError::Negative),
+            0 => Err(CreationError::Zero),
+            _ => Ok(PositiveNonzeroInteger(value as u64))
+        }
+        
+    }
+}
+
+#[test]
+fn test_creation() {
+    assert!(PositiveNonzeroInteger::new(10).is_ok());
+    assert_eq!(
+        Err(CreationError::Negative),
+        PositiveNonzeroInteger::new(-10)
+    );
+    assert_eq!(Err(CreationError::Zero), PositiveNonzeroInteger::new(0));
+}
+*/
+
