98c81fd04ead175062eddf9b41a8d8c6160c92bd
Aidan K <ak@umorpha.io>
2024-01-17 16:43:30 -0700

    propigating errors hw1 and did a bit of file / result playing around 	new file:   Intermediate/understanding error handling in rust/propagating_errors/example.txt 	new file:   Intermediate/understanding error handling in rust/propagating_errors/src/hw1.rs 	modified:   Intermediate/understanding error handling in rust/propagating_errors/src/main.rs

--- Intermediate/understanding error handling in rust/propagating_errors/src/main.rs
+++ Intermediate/understanding error handling in rust/propagating_errors/src/main.rs
@@ -3,7 +3,15 @@ use::std::{io::{self, Read}, fs::File};
 
 fn main() {
 
-    let contents = read_file("example.txt");
+    //let contents = read_file("example.txt");
+    let contents = match read_file("example.txt") {
+        Ok(c) => c,
+        Err(error) => panic!("error reading file: {}", error)
+    };
+
+    find("this", contents.as_str());
+
+    
 }
 
 fn read_file(filename: &str) -> Result<String, io::Error> {
@@ -11,4 +19,12 @@ fn read_file(filename: &str) -> Result<String, io::Error> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
     Ok(contents)
+}
+
+fn find(query: &str, data: &str) {
+    for i in data.lines() {
+        if i.contains(query) {
+            println!("{}", i.to_string());
+        }
+    }
 }
