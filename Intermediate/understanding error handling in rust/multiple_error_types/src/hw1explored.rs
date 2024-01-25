60348ff7258945105c93ec2f800c7d37a6c2b597
Aidan K <ak@umorpha.io>
2024-01-25 16:38:39 -0700

    explored hw1 a bit

--- Intermediate/understanding error handling in rust/multiple_error_types/src/hw1explored.rs
+++ Intermediate/understanding error handling in rust/multiple_error_types/src/hw1explored.rs
@@ -5,13 +5,20 @@ use std::fmt;
 use std::num::ParseIntError;
 
 // TODO: update the return type of `main()` to make this compile.
-fn main() -> Result<(), Box<dyn error::Error>> {
+fn main() {
     let pretend_user_input = "1";
-    let x: i64 = pretend_user_input.parse()?;
-    println!("output={:?}", PositiveNonzeroInteger::new(x)?);
-    Ok(())
+    let parsed = input_parser(pretend_user_input);
+    match parsed {
+        Ok(i) => println!("output is {:?}", i),
+        Err(e) => println!("Error: {e}")
+    };
 }
 
+fn input_parser(input: &str) -> Result<PositiveNonzeroInteger, Box<dyn error::Error>> {
+    let x: i64 = input.parse()?;
+    let y = PositiveNonzeroInteger::new(x)?;
+    Ok(y)
+}
 // Don't change anything below this line.
 
 #[derive(PartialEq, Debug)]
